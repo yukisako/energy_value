07365aa8d67a19475541d9d747918a316df8184c
yukisako <yukisakoatoz@gmail.com>
2015-12-23 20:09:01 +0900

    initial commit

--- Decimal Change/ViewController.swift
+++ Decimal Change/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var result: UILabel!
+    
+    @IBOutlet var input: UITextField!
+    
+    @IBOutlet var nuclearPower: UILabel!
+    
+    @IBAction func submit(sender: AnyObject) {
+        //ここにchangeボタンが押された時の処理
+        let weight = Int(input.text!)!
+        
+        let energy = weight * 299792458 * 299792458
+        
+        let nuclear = energy / (1000000 * 1000 * 3600 * 24 * 365)
+        
+        result.text = "\(energy) Jです．"
+        
+        nuclearPower.text = "原発\(nuclear) 台が1年に作るエネルギー"
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
