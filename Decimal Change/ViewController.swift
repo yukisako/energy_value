416c60fa0bdfae5c29b1654562eb631434a8aed6
yukisako <yukisakoatoz@gmail.com>
2015-12-23 17:28:04 +0900

    Initial Commit

---
+++ Decimal Change/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Decimal Change
+//
+//  Created by 迫 佑樹 on 2015/12/23.
+//  Copyright © 2015年 迫 佑樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
